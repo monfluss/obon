9e225fcd078fd10662ea954164e84b0962b5cb3d
mondfluss <kgbekbossov@gmail.com>
2026-06-17 06:48:48 +0600

    Исправление синтаксической ошибки в settings

--- core/settings.py
+++ core/settings.py
@@ -237,6 +237,6 @@ JAZZMIN_UI_TWEAKS = {
     },
  }
 
-STATIC_URL = '/static/',
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
